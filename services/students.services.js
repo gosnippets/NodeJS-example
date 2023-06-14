3fad9f3cff09e5e858a1a0f3c118200eab812f9b
Ramdas Gupta <er.rdgupta@gmail.com>
2023-06-14 09:37:50 +0530

    Users CRUD Example

--- services/students.services.js
+++ services/students.services.js
@@ -1,47 +1,48 @@
 import { createStudent, deleteStudent, findStudentByID, findStudents, updateStudent } from "../models/students.models.js"
+import msg from "../utils/response.js"
 
 const _createStudent = async (req, res, next) => {
     try {
         const student = await createStudent(req.body);
-        res.status(200).json({ status: "Student created successfully", students: student })
+        return msg.successMsg(res, 201, student, "Student created successfully");
     } catch (error) {
-        console.log("Error..", error)
+        return msg.errorMsg(res, 503, error);
     }
 }
 
 const _getAllStudents = async (req, res, next) => {
     try {
         const students = await findStudents();
-        res.status(200).json({ status: "Students returned successfully", students: students })
+        return msg.successMsg(res, 200, students, "Students returned successfully");
     } catch (error) {
-        console.log("Error..", error)
+        return msg.errorMsg(res, 503, error);
     }
 }
 
-const _getStudentById = async (req, res, next) => {   
+const _getStudentById = async (req, res, next) => {
     try {
         const student = await findStudentByID(req.params.id);
-        res.status(200).json({ status: "Student returned successfully", student: student })
+        return msg.successMsg(res, 200, student,  "Student returned successfully");
     } catch (error) {
-        console.log("Error..", error)
+        return msg.errorMsg(res, 503, error);
     }
 }
 
 const _updateStudent = async (req, res, next) => {
     try {
         const result = await updateStudent(req.params.id, req.body)
-        res.status(200).json({ status: "Student updated successfully", student: result })
+        return msg.successMsg(res, 200, result, "Student updated successfully");
     } catch (error) {
-        console.log("Error..", error)
+        return msg.errorMsg(res, 503, error);
     }
 }
 
 const _deleteStudent = async (req, res, next) => {
     try {
         const student = await deleteStudent(req.params.id);
-        res.status(200).json({ status: "Student deleted successfully", student: student })
+        return msg.successMsg(res, 200, student, "Student deleted successfully");
     } catch (error) {
-        console.log("Error..", error)
+        return msg.errorMsg(res, 503, error);
     }
 }
 
