7fe2da206b2da6bd3fd10cc065f6629a80d7721c
Ramdas Gupta <er.rdgupta@gmail.com>
2023-07-06 09:39:59 +0530

    blog post

--- BlogPostExample/app/services/category.services.js
+++ BlogPostExample/app/services/category.services.js
@@ -2,13 +2,14 @@ import * as dotenv from "dotenv";
 import db from "../models/index.js";
 import msg from "../utils/response.js"
 dotenv.config();
-const Category = db.category
+const Category = db.category;
+const Post = db.post;
 
 async function _createCategory(req, res) {
     try {
         const { title } = req.body;
         const data = await Category.create({ title });
-        return msg.successMsg(res, 201, data, "Category created successfully...")
+        return msg.successMsg(res, 201, data, "Category created successfully...");
     } catch (error) {
         return msg.errorMsg(res, 500, error.message || "Something went wrong");
     }
@@ -16,7 +17,8 @@ async function _createCategory(req, res) {
 
 const _getAllCategorys = async (req, res) => {
     try {
-
+        const data = await Category.findAll({ include: Post })
+        return msg.successMsg(res, 201, data, "Categories returned successfully...");
     } catch (error) {
         return msg.errorMsg(res, 500, error.message || "Something went wrong");
     }
@@ -24,7 +26,9 @@ const _getAllCategorys = async (req, res) => {
 
 async function _getCategoryById(req, res) {
     try {
-
+        const { id } = req.params;
+        const category = await Category.findByPk(id, { include: Post })
+        return msg.successMsg(res, 201, category, "Category returned successfully...");
     } catch (error) {
         return msg.errorMsg(res, 500, error.message || "Something went wrong");
     }
--- BlogPostExample/app/services/post.services.js
+++ BlogPostExample/app/services/post.services.js
@@ -3,6 +3,7 @@ import db from "../models/index.js";
 import msg from "../utils/response.js"
 dotenv.config();
 const Post = db.post
+const User = db.user
 const Category = db.category
 
 async function _createPost(req, res) {
@@ -10,12 +11,12 @@ async function _createPost(req, res) {
         const { title, description, UserId, categories } = req.body;
         const postData = await Post.create({ title, description, UserId });
 
-        for(var categoryId of categories){
+        for (var categoryId of categories) {
             const category = await Category.findByPk(categoryId);
-            const data = await postData.addCategory(category);
+            postData.addCategory(category);
         }
 
-        return msg.successMsg(res, 201, postData, "Post returned successfully...")
+        return msg.successMsg(res, 201, postData, "Post created successfully...")
     } catch (error) {
         console.log("error", error);
         return msg.errorMsg(res, 500, error.message || "Something went wrong");
@@ -24,7 +25,8 @@ async function _createPost(req, res) {
 
 const _getAllPosts = async (req, res) => {
     try {
-
+        const data = await Post.findAll({ include: [{ model: User, attributes: { exclude: ['password'] } }, { model: Category, attributes: ['title'] }] })
+        return msg.successMsg(res, 200, data, "Post returned successfully...");
     } catch (error) {
         return msg.errorMsg(res, 500, error.message || "Something went wrong");
     }
@@ -32,7 +34,10 @@ const _getAllPosts = async (req, res) => {
 
 async function _getPostById(req, res) {
     try {
+        const { id } = req.params;
+        const post = await Post.findByPk(id, { include: [{ model: User, attributes: { exclude: ['password'] } }, { model: Category, attributes: ['title'] }] });
 
+        return msg.successMsg(res, 200, post, "Post returned successfully...");
     } catch (error) {
         return msg.errorMsg(res, 500, error.message || "Something went wrong");
     }
